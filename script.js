67ea57af0158f094f04fe0fe5e27ec1298c766bf
downlouder <118766413+downlouder@users.noreply.github.com>
2023-02-15 02:55:59 +0200

    Design

---
+++ script.js
@@ -0,0 +1,33 @@
+const zero = document.getElementById('0');
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+
+const equal = document.getElementById('equal');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const multiple = document.getElementById('multiple');
+const divide = document.getElementById('divide');
+const clear = document.getElementById('ac');
+const erase = document.getElementById('erase');
+const dat = document.getElementById('dat');
+const percents = document.getElementById('percents');
+const brackets = document.getElementById('brackets');
+
+function addNumbers(...num) {
+
+}
+
+function calculator() {
+    addNumbers();
+    subsNumbers();
+    multiplyNumbers();
+    divideNumbers();
+    sum();
+}
